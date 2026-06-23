cd502736ba2509d0b03cb932845d1929e885b13f
Anonim <kadal.rigel@gmail.com>
2026-06-23 23:31:52 +0700

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,12 +4,3 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function formatPrice(price: number, currency: string = 'IDR') {
-  return new Intl.NumberFormat('id-ID', {
-    style: 'currency',
-    currency: currency,
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-  }).format(price);
-}
