a3c42c63976a564a1aa6fefb79473e6f87d20af9
Anonim <kadal.rigel@gmail.com>
2026-06-24 00:00:44 +0700

    Update galaxy-scene-content.tsx

--- components/three/galaxy-scene-content.tsx
+++ components/three/galaxy-scene-content.tsx
@@ -2,8 +2,10 @@
 
 import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { CSS2DRenderer, CSS2DObject } from 'three/addons/renderers/CSS2DRenderer.js';
+// @ts-ignore
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+// @ts-ignore
+import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer.js';
 
 interface Asset {
   label: string;
