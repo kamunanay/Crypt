3efbba5e7d980a6a75f919d8def6267ae566c2aa
Anonim <kadal.rigel@gmail.com>
2026-06-24 10:43:35 +0700

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,19 +5,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function formatPrice(price: number, currency: string = 'IDR') {
-  return new Intl.NumberFormat('id-ID', {
-    style: 'currency',
-    currency: currency,
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-  }).format(price);
-}
-
-export function formatChange(change: number) {
-  return `${change >= 0 ? '+' : ''}${change.toFixed(2)}%`;
-}
-
 export function formatNumber(num: number): string {
   return new Intl.NumberFormat('id-ID').format(num);
 }
