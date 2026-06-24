49efe7957cde8ecfa8870cab98f9ce1f8ce71d11
Anonim <kadal.rigel@gmail.com>
2026-06-24 10:50:41 +0700

    Update page.tsx

--- app/currency/[symbol]/page.tsx
+++ app/currency/[symbol]/page.tsx
@@ -1,16 +1,7 @@
 import CurrencyDetailClient from './CurrencyDetailClient';
 
 export async function generateStaticParams() {
-  return [
-    { symbol: 'usd' },
-    { symbol: 'eur' },
-    { symbol: 'gbp' },
-    { symbol: 'jpy' },
-    { symbol: 'idr' },
-    { symbol: 'aud' },
-    { symbol: 'chf' },
-    { symbol: 'cny' },
-  ];
+  return [{ symbol: 'usd' }, { symbol: 'eur' }, { symbol: 'gbp' }, { symbol: 'jpy' }, { symbol: 'idr' }, { symbol: 'aud' }, { symbol: 'chf' }, { symbol: 'cny' }];
 }
 
 export default function Page({ params }: { params: any }) {
