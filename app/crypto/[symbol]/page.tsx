e5c81c6a6108bb2adca854ba732232b9f746fd58
Anonim <kadal.rigel@gmail.com>
2026-06-25 16:10:28 +0700

    Delete app/crypto/[symbol]/page.tsx

--- app/crypto/[symbol]/page.tsx
+++
@@ -1,9 +0,0 @@
-import CryptoDetailClient from './CryptoDetailClient';
-
-export async function generateStaticParams() {
-  return [{ symbol: 'btc' }, { symbol: 'eth' }, { symbol: 'sol' }, { symbol: 'xrp' }, { symbol: 'bnb' }];
-}
-
-export default function Page({ params }: { params: any }) {
-  return <CryptoDetailClient symbol={params.symbol} />;
-}
