cfc34f667e9c580d44036d4559bdccd462a0e9c7
Anonim <kadal.rigel@gmail.com>
2026-06-25 16:10:46 +0700

    Create page.tsx

---
+++ app/crypto/[symbol]/page.tsx
@@ -0,0 +1,9 @@
+import CryptoDetailClient from './CryptoDetailClient';
+
+export async function generateStaticParams() {
+  return [{ symbol: 'btc' }, { symbol: 'eth' }, { symbol: 'sol' }, { symbol: 'xrp' }, { symbol: 'bnb' }];
+}
+
+export default function Page({ params }: { params: any }) {
+  return <CryptoDetailClient symbol={params.symbol} />;
+}
